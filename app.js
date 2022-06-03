e8444ab8b68758787d4d4001c4530fbfff07ab99
luisfernandob87 <luisfernandob87@gmail.com>
2022-06-02 19:38:39 -0600

    project 50%

---
+++ app.js
@@ -0,0 +1,6 @@
+function menuMobile() {
+    const nav = document.getElementById('nav');
+    const menu = document.getElementById('menu')
+    nav.classList.toggle('hidden')
+    menu.classList.toggle('rotate')
+}
